7f3e2ca3d5043120cc0a10ef9130617b42c5bd35
Mike Holzbach <maholzbach@gmail.com>
2020-04-27 08:25:09 -0400

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App/App";
+
+const title = "CoinBase";
+
+ReactDOM.render(<App title={title} />, document.getElementById("app"));
+
+module.hot.accept();
