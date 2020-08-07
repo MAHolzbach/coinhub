ce8b0377729c46d9ae9eb7c76d332bb91d60c3fa
Mike Holzbach <maholzbach@gmail.com>
2020-08-07 12:00:16 -0400

    Header component done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React, { useContext } from "react";
-import { AppContext } from "../App/App";
+import React from "react";
+
 import "./header.scss";
 
+import SearchIcon from "../../img/search-icon.png";
+import AlertsIcon from "../../img/alerts-icon.png";
+import AccountIcon from "../../img/account-icon.png";
+
 const Header = () => {
-  const context = useContext(AppContext);
-  return <div></div>;
+  return (
+    <div className="header-wrapper">
+      <img src={SearchIcon} alt="search" className="header-wrapper__icon" />
+      <img src={AlertsIcon} alt="alerts" className="header-wrapper__icon" />
+      <img
+        src={AccountIcon}
+        alt="alerts"
+        className="header-wrapper__icon header-wrapper__user-icon"
+      />
+      <div className="header-wrapper__user-info">
+        <p className="user-info__text user-info__name">John Doe</p>
+        <p className="user-info__text user-info__title">Admin</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
